dfafd161009ae605d9b0b2ba3b7903439a4b9e92
Peter Laraia <PeterLaraiaGit@gmail.com>
2017-07-10 00:28:12 -0700

    fix: use scoped name in flatModuleId

--- tools/config/rollup/rollup.umd.config.js
+++ tools/config/rollup/rollup.umd.config.js
@@ -2,10 +2,5 @@ export default {
     entry: './build/index.js',
     dest: './dist/bundles/media-query.umd.js',
     format: 'umd',
-    moduleName: 'media-query',
-    external: [
-        '@angular/core',
-        '@angular/common',
-        'rxjs'
-    ]
+    moduleName: 'media-query'
 }
